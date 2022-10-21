9bbb65bbc0120d035daf8ef05b1d47db07e2dfe3
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-21 16:23:33 +0900

    create admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Review, Comment
 
 # Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'review', )
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
